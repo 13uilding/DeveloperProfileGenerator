d5707dae43071792509b3e21045b0a3e4be260a6
deebs <hereisdanbrown@gmail.com>
2020-01-14 19:29:24 -0700

    Added an axios call

--- Class-Content_09-NodeJS_02-Homework_Develop_index.js
+++ Class-Content_09-NodeJS_02-Homework_Develop_index.js
@@ -1,3 +1,27 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+  axios
+    .get(queryUrl)
+    .then(function(respond) {
+      // console.log(respond.data);
+      const repoArr = respond.data.map(function(repo){
+        return repo.name;
+      })
+      console.log(repoArr);
+    });
+  });
+
 const questions = [
   
 ];
@@ -9,3 +33,5 @@ function writeToFile(fileName, data) {
 function init() {
 
 init();
+
+
